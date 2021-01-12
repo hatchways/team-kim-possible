49b8857f9e80cce7dc082b5233b8f2b96c8986e8
Alex Setia <alextheprogrammer21@gmail.com>
2021-01-11 23:44:15 -0500

    add catch error handling to db connection

--- server/app.js
+++ server/app.js
@@ -10,10 +10,13 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
   })
   .then(() => {
     console.log("DB Connected!");
-  });
+  })
+  .catch((err) => console.error(err));
 
 const indexRouter = require("./routes/index");
 const pingRouter = require("./routes/ping");
