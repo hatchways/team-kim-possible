b1c125efd0725b40691c3b039faf3e7dc10be772
Chris Kim <chrisdjkim@gmail.com>
2021-01-16 09:22:57 +0900

    Made dropbox a bit prettier
    Added some styling to dropbox component. Also made it a grid item for integration into other grids.

--- client/src/components/dropbox.component.js
+++ client/src/components/dropbox.component.js
@@ -1,8 +1,21 @@
 import React, { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import Grid from "@material-ui/core/Grid";
 import axios from "axios";
 
+const useStyles = makeStyles({
+  dropBoxContainer: {
+    padding: "10px",
+    margin: "20px",
+    textAlign: "center",
+    border: "1px dotted",
+  },
+});
+
 function DropBox() {
+  const classes = useStyles();
   const onDrop = useCallback(async (acceptedFiles) => {
     if (acceptedFiles.length > 0) {
       const formData = new FormData();
@@ -14,7 +27,6 @@ function DropBox() {
         headers: { "Content-Type": "multipart/form-data" },
       });
     }
-    console.log("Please add one file at a time!");
   }, []);
   //Only allows one but will return empty array if multiple files dropped
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
@@ -23,14 +35,16 @@ function DropBox() {
   });
 
   return (
-    <div {...getRootProps()}>
+    <Grid item className={classes.dropBoxContainer} {...getRootProps()}>
       <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag and drop some files here, or click to select files</p>
-      )}
-    </div>
+      <InputLabel>
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag and drop some files here, or click to select files</p>
+        )}
+      </InputLabel>
+    </Grid>
   );
 }
 
--- server/models/user.models.js
+++ server/models/user.models.js
@@ -28,7 +28,6 @@ const userSchema = mongoose.Schema({
   },
   profilePictureUrl: {
     type: String,
-    unique: true,
   },
 });
 
