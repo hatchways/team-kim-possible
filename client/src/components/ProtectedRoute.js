ff4c215f94c2a6e1833ed57f25d51913bd9f06fb
Jr <richard.beltranjr@gmail.com>
2021-01-20 17:38:46 -0500

    removed comments

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,23 +1,6 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 
-// function ProtectedRoute(props) {
-// 	const renderComponent = () => {
-// 		if (localStorage.loggedIn) {
-// 			return props.component;
-// 		} else {
-// 			return <Redirect to={"/"}></Redirect>;
-// 		}
-// 	};
-// 	return (
-// 		<Route exact to={props.to}>
-// 			{() => renderComponent()}
-// 		</Route>
-// 	);
-// }
-
-// export default ProtectedRoute;
-
 function ProtectedRoute({ component: Component, ...rest }) {
 	return (
 		<Route
