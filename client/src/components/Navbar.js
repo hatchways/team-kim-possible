15dc5fc4109249a259fc7ef6fcfc5e8bcf52169b
Chris Kim <chrisdjkim@gmail.com>
2021-02-03 08:13:09 -0500

    Updated some comments

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -71,8 +71,8 @@ export default function ButtonAppBar() {
     window.addEventListener("resize", () => setView());
   }, []);
 
+  //Functions for opening or closing menu (mobile)
   const openDrawer = () => setState((prev) => ({ ...prev, drawerOpen: true }));
-
   const closeDrawer = () =>
     setState((prev) => ({ ...prev, drawerOpen: false }));
 
@@ -128,6 +128,7 @@ export default function ButtonAppBar() {
     </div>
   );
 
+  //standard desktop menu
   const menu = (
     <AppBar position="static" className={classes.navbar} elevation={5}>
       <Toolbar>
@@ -152,7 +153,7 @@ export default function ButtonAppBar() {
         <Link to="/carrental" className={classes.textDecoration}>
           <Button className={classes.links}>Cars</Button>
         </Link>
-        <Link to={"/userpage"}>
+        <Link to="/userpage">
           <Avatar src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2ee38b4e-1ad8-433e-b83b-5f1492547b2d/d3a4k16-486f7e33-afd1-4d1d-a573-ff3d9ea74e8f.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMmVlMzhiNGUtMWFkOC00MzNlLWI4M2ItNWYxNDkyNTQ3YjJkXC9kM2E0azE2LTQ4NmY3ZTMzLWFmZDEtNGQxZC1hNTczLWZmM2Q5ZWE3NGU4Zi5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.g16PZQ0d4y8s6zV_SIhLryhy9X6UXCC0ZMN0mIW35Uk" />
         </Link>
       </Toolbar>
