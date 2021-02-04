d649caf6e38e21f91616a8947ce5686339ef7b32
Chloe <chloe.ng.1292@gmail.com>
2021-02-04 10:31:31 +0700

    rou

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,26 +3,33 @@ const jwt = require("jsonwebtoken");
 const { User } = require("../models/user.models");
 
 const auth = async (req, res, next) => {
-	try {
-		if (!req.cookies.authenticationToken) {
-			return res
-				.status(401)
-				.send({ responseMessage: "Authentication error!" });
-		}
+  try {
+    // if (!req.cookies.authenticationToken) {
+    // 	return res
+    // 		.status(401)
+    // 		.send({ responseMessage: "Authentication error!" });
+    // }
+    // const decodedToken = jwt.verify(
+    // 	req.cookies.authenticationToken,
+    // 	process.env.SECRET_KEY
+    // );
+    if (!req.headers.cookie) {
+      return res.status(401).send({ responseMessage: "Authentication error!" });
+    }
 
-		const decodedToken = jwt.verify(
-			req.cookies.authenticationToken,
-			process.env.SECRET_KEY
-		);
-		const userId = decodedToken.userId;
-		const user = await User.findOne({ _id: userId });
-		//Allows access of user object in authenticated routes by accessing req.user
-		req.user = user;
+    const decodedToken = jwt.verify(
+      req.headers.cookie.split("=")[1],
+      process.env.SECRET_KEY
+    );
+    const userId = decodedToken.userId;
+    const user = await User.findOne({ _id: userId });
+    //Allows access of user object in authenticated routes by accessing req.user
+    req.user = user;
 
-		next();
-	} catch (e) {
-		res.status(401).send({ responseMessage: `Authentication error: ${e}` });
-	}
+    next();
+  } catch (e) {
+    res.status(401).send({ responseMessage: `Authentication error: ${e}` });
+  }
 };
 
 module.exports = auth;
--- server/routes/favorites.js
+++ server/routes/favorites.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { User } = require("../models/user.models");
+const { Location } = require("../models/location.models");
 
 //PUT /favorites
 router.put("/", async function (req, res, next) {
@@ -20,4 +21,31 @@ router.get("/getAllFavorites", async function (req, res, next) {
   res.status(200).send(user.favorites);
 });
 
+router.post("/", async function (req, res, next) {
+  const { favorites } = req.body;
+  try {
+    const user = await User.findById(req.user._id);
+    async function getFavorites(arr) {
+      let promises = arr.map(async (i) => {
+        return await Location.findOne({ location: i });
+      });
+      return await Promise.all(promises);
+    }
+    let result = await getFavorites(favorites);
+
+    user.favorites = result;
+
+    await user.save();
+    res.status(200).json({
+      user: {
+        name: user.name,
+        email: user.email,
+        home: user.home,
+        favorites: result,
+      },
+    });
+  } catch (err) {
+    res.send(err);
+  }
+});
 module.exports = router;
