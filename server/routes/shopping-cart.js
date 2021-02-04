55bb4b314881b39235dc341d626b1e349fb79948
Alex Setia <alextheprogrammer21@gmail.com>
2021-02-04 00:41:39 -0500

    modify shopping cart

--- server/routes/shopping-cart.js
+++ server/routes/shopping-cart.js
@@ -7,67 +7,6 @@ const { Cars } = require("../models/cars.models");
 const { Flights } = require("../models/flights.models");
 
 router.post("/update-cart", async function (req, res, next) {
-  // const nullId = "000000000000000000000000";
-  // for (index in req.body) {
-  //   if (req.body[index].type === "car") {
-  //     const car = await Cars.findOne({
-  //       name: req.body[index].name,
-  //     });
-  //   }
-
-  //   if (req.body[index].type === "hotel") {
-  //     const hotel = await Hotels.findOne({
-  //       name: req.body[index].name,
-  //     });
-
-  //     await User.findOneAndUpdate(
-  //       { _id: objectId("601b581c8fff800580ec3554") },
-  //       {
-  //         shoppingCart: {
-  //           hotel: hotel._id,
-  //         },
-  //       }
-  //     );
-  //   }
-
-  //   if (req.body[index].type === "flight") {
-  //     const flightData = {
-  //       departureDate: "2021-01-01",
-  //       returnDate: "2021-01-02",
-  //       departureLocation: req.body[index].originPlace,
-  //       destinationLocation: req.body[index].destinationPlace,
-  //       price: req.body[index].price,
-  //     };
-
-  //     const flight = new Flights(flightData);
-  //     await flight.save();
-
-  //     await User.findOneAndUpdate(
-  //       { _id: objectId("601b581c8fff800580ec3554") },
-  //       {
-  //         shoppingCart: {
-  //           flight: flight._id,
-  //         },
-  //       }
-  //     );
-  //   }
-  // }
-  // const carBool = false;
-  // const hotelBool = false;
-  // const flightBool = false;
-
-  // for (index in req.body) {
-  //   if (req.body[index].type === "car") {
-  //     carBool = true;
-  //   }
-  //   if (req.body[index].type === "hotel") {
-  //     hotelBool = true;
-  //   }
-  //   if (req.body[index].type === "flight") {
-  //     flightBool = true;
-  //   }
-  // }
-
   const car = await Cars.findOne({
     name: "Honda",
   });
