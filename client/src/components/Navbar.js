e3ea7e8731ca91c2be5ce9c965c01350535e3160
Jr <richard.beltranjr@gmail.com>
2021-02-05 16:46:45 -0500

    fix navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -13,6 +13,10 @@ import {
   MenuItem,
 } from "@material-ui/core";
 
+import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
+import { Popover } from "@material-ui/core";
+import ShoppingCartPopoverContent from "./ShoppingCart/ShoppingCartPopoverContent";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -62,11 +66,22 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ButtonAppBar(props) {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
   const [state, setState] = useState({
     mobile: false,
     drawerOpen: false,
   });
   const { mobile, drawerOpen } = state;
+
+  const handleCloseShoppingCart = () => {
+    setAnchorEl(null);
+  };
+
+  const handleOpenShoppingCart = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
 
   useEffect(() => {
     const setView = () => {
@@ -86,50 +101,67 @@ export default function ButtonAppBar(props) {
   const drawerItems = (
     <div className={classes.drawerContainer}>
       <Link
-        to="/"
+        to='/'
         className={classes.textDecoration}
-        onClick={() => closeDrawer()}
-      >
+        onClick={() => closeDrawer()}>
         <MenuItem className={classes.drawerItem}>Travel Booking</MenuItem>
       </Link>
 
       <Link
-        to="explore"
+        to='explore'
         className={classes.textDecoration}
-        onClick={() => closeDrawer()}
-      >
+        onClick={() => closeDrawer()}>
         <MenuItem className={classes.drawerItem}>Explore</MenuItem>
       </Link>
 
       <Link
-        to="/"
+        to='/'
         className={classes.textDecoration}
-        onClick={() => closeDrawer()}
-      >
+        onClick={() => closeDrawer()}>
         <MenuItem className={classes.drawerItem}>Flights</MenuItem>
       </Link>
 
       <Link
-        to="/hotels"
+        to='/hotels'
         className={classes.textDecoration}
-        onClick={() => closeDrawer()}
-      >
+        onClick={() => closeDrawer()}>
         <MenuItem className={classes.drawerItem}>Hotels</MenuItem>
       </Link>
 
       <Link
-        to="/carrental"
+        to='/carrental'
         className={classes.textDecoration}
-        onClick={() => closeDrawer()}
-      >
+        onClick={() => closeDrawer()}>
         <MenuItem className={classes.drawerItem}>Cars</MenuItem>
       </Link>
 
+      <ShoppingCartOutlinedIcon
+        className={classes.shoppingCartIcon}
+        onClick={(e) => handleOpenShoppingCart(e)}
+      />
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={(e) => handleCloseShoppingCart(e)}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}>
+        <ShoppingCartPopoverContent cbExit={handleCloseShoppingCart} />
+      </Popover>
+      <Link to={"/userpage"}>
+        <Avatar src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2ee38b4e-1ad8-433e-b83b-5f1492547b2d/d3a4k16-486f7e33-afd1-4d1d-a573-ff3d9ea74e8f.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMmVlMzhiNGUtMWFkOC00MzNlLWI4M2ItNWYxNDkyNTQ3YjJkXC9kM2E0azE2LTQ4NmY3ZTMzLWFmZDEtNGQxZC1hNTczLWZmM2Q5ZWE3NGU4Zi5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.g16PZQ0d4y8s6zV_SIhLryhy9X6UXCC0ZMN0mIW35Uk' />
+      </Link>
+
       <Link
-        to="/userpage"
+        to='/userpage'
         className={classes.textDecoration}
-        onClick={() => closeDrawer()}
-      >
+        onClick={() => closeDrawer()}>
         <MenuItem className={classes.drawerItem}>User</MenuItem>
       </Link>
     </div>
@@ -137,47 +169,65 @@ export default function ButtonAppBar(props) {
 
   //standard desktop menu
   const menu = (
-    <AppBar position="static" className={classes.navbar} elevation={5}>
+    <AppBar position='static' className={classes.navbar} elevation={5}>
       <Toolbar>
-        <Typography variant="h6" className={classes.title}>
-          <Link className={classes.navTitle} to="/">
+        <Typography variant='h6' className={classes.title}>
+          <Link className={classes.navTitle} to='/'>
             Travel Booking
           </Link>
         </Typography>
-        <Link to="/explore" className={classes.textDecoration}>
+        <Link to='/explore' className={classes.textDecoration}>
           <Button className={classes.links}>
-            <Typography variant="button">Explore</Typography>
+            <Typography variant='button'>Explore</Typography>
           </Button>
         </Link>
-        <Link to="/" className={classes.textDecoration}>
+        <Link to='/' className={classes.textDecoration}>
           <Button className={classes.links}>
-            <Typography variant="button">Flights</Typography>
+            <Typography variant='button'>Flights</Typography>
           </Button>
         </Link>
-        <Link to="/hotels" className={classes.textDecoration}>
+        <Link to='/hotels' className={classes.textDecoration}>
           <Button className={classes.links}>Hotels</Button>
         </Link>
-        <Link to="/carrental" className={classes.textDecoration}>
+        <Link to='/carrental' className={classes.textDecoration}>
           <Button className={classes.links}>Cars</Button>
         </Link>
-        <Link to="/userpage">
-          <Avatar src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2ee38b4e-1ad8-433e-b83b-5f1492547b2d/d3a4k16-486f7e33-afd1-4d1d-a573-ff3d9ea74e8f.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMmVlMzhiNGUtMWFkOC00MzNlLWI4M2ItNWYxNDkyNTQ3YjJkXC9kM2E0azE2LTQ4NmY3ZTMzLWFmZDEtNGQxZC1hNTczLWZmM2Q5ZWE3NGU4Zi5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.g16PZQ0d4y8s6zV_SIhLryhy9X6UXCC0ZMN0mIW35Uk" />
+        <ShoppingCartOutlinedIcon
+          className={classes.shoppingCartIcon}
+          onClick={(e) => handleOpenShoppingCart(e)}
+        />
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={(e) => handleCloseShoppingCart(e)}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}>
+          <ShoppingCartPopoverContent cbExit={handleCloseShoppingCart} />
+        </Popover>
+        <Link to='/userpage'>
+          <Avatar src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2ee38b4e-1ad8-433e-b83b-5f1492547b2d/d3a4k16-486f7e33-afd1-4d1d-a573-ff3d9ea74e8f.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMmVlMzhiNGUtMWFkOC00MzNlLWI4M2ItNWYxNDkyNTQ3YjJkXC9kM2E0azE2LTQ4NmY3ZTMzLWFmZDEtNGQxZC1hNTczLWZmM2Q5ZWE3NGU4Zi5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.g16PZQ0d4y8s6zV_SIhLryhy9X6UXCC0ZMN0mIW35Uk' />
         </Link>
       </Toolbar>
     </AppBar>
   );
 
   const menuMobile = (
-    <AppBar position="static" className={classes.navbar} elevation={5}>
+    <AppBar position='static' className={classes.navbar} elevation={5}>
       <IconButton onClick={openDrawer}>
         <MenuIcon />
       </IconButton>
       <Drawer
-        anchor="top"
+        anchor='top'
         open={drawerOpen}
         onClose={closeDrawer}
-        className={classes.drawer}
-      >
+        className={classes.drawer}>
         {drawerItems}
       </Drawer>
     </AppBar>
